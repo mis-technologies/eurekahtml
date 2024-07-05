9742b54acb08cb5180cd86f6a9d76db5a61cccdc
Joseph <mrrfabulous8@gmail.com>
2024-07-05 19:14:24 +0100

    added the programs page, contact and FAQ

--- main.js
+++ main.js
@@ -168,3 +168,21 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("window resize, please refresh page");
   });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const accordionHeaders = document.querySelectorAll('.accordion-header');
+
+  accordionHeaders.forEach(header => {
+    header.addEventListener('click', () => {
+      const content = header.nextElementSibling;
+
+      header.classList.toggle('active');
+
+      if (content.style.display === 'block') {
+        content.style.display = 'none';
+      } else {
+        content.style.display = 'block';
+      }
+    });
+  });
+});
