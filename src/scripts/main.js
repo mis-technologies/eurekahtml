d927df5695930e4f04cd6c2bf8c56c3fbf123cc5
Joseph <mrrfabulous8@gmail.com>
2024-07-22 10:34:43 +0100

    implementing tailwind css

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,14 @@
-window.addEventListener("scroll", function () {
-  var navbar = document.querySelector(".container");
-  var navBarOffset = navbar.offsetTop;
-
-  if (window.pageYOffset >= navBarOffset) {
-    navbar.classList.add("fixed-nav");
-  } else {
-    navbar.classList.remove("fixed-nav");
-  }
-});
-
+// When the DOM is loaded, execute this code
 document.addEventListener("DOMContentLoaded", () => {
+  // Nav bar script
   const openMenu = document.getElementById("openMenu");
   const closeMenu = document.getElementById("closeMenu");
-  const navBar = document.getElementById("nav-bar");
+  const navBar = document.getElementById("mobileNav");
   const navLinks = document.querySelectorAll(".nav-items");
 
   openMenu.addEventListener("click", () => {
-    navBar.style.display = "flex";
+    // navBar.style.display = "flex";
+    navBar.classList.toggle("navOpen");
   });
 
   closeMenu.addEventListener("click", () => {
@@ -37,9 +29,10 @@ document.addEventListener("DOMContentLoaded", () => {
       navBar.style.display = "flex";
     }
   });
+  //  End of Nav bar script
 
+  // Card effect for the Event section
   const cards = document.querySelectorAll(".card");
-
   const card1 = document.getElementById("card-1");
   const card2 = document.getElementById("card-2");
   const card3 = document.getElementById("card-3");
@@ -49,6 +42,7 @@ document.addEventListener("DOMContentLoaded", () => {
     card.classList.add("row-2");
   });
 
+  // card effect on bigger screens
   if (window.innerWidth > 800) {
     // card 1
     card1.addEventListener("mouseover", () => {
@@ -167,22 +161,81 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     console.log("window resize, please refresh page");
   });
-});
+  // End of card hover effect for the event section
 
-document.addEventListener('DOMContentLoaded', () => {
-  const accordionHeaders = document.querySelectorAll('.accordion-header');
+  // Accordain Section
+  const accordionHeaders = document.querySelectorAll(".accordion-header");
 
-  accordionHeaders.forEach(header => {
-    header.addEventListener('click', () => {
+  accordionHeaders.forEach((header) => {
+    header.addEventListener("click", () => {
       const content = header.nextElementSibling;
 
-      header.classList.toggle('active');
+      header.classList.toggle("active");
 
-      if (content.style.display === 'block') {
-        content.style.display = 'none';
+      if (content.style.display === "block") {
+        content.style.display = "none";
       } else {
-        content.style.display = 'block';
+        content.style.display = "block";
       }
     });
   });
+
+  // End of DomContentLoaded
 });
+
+// Scrool to top function
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
+// Message Form
+const checkMessage = (formId) => {
+  const form = document.getElementById(formId);
+  const name = form.querySelector(`input[name="name"]`).value;
+  const email = form.querySelector(`input[name="email"]`).value;
+  const phoneNumber = form.querySelector(`input[name="phone"]`).value;
+  const subject = form.querySelector(`input[name="subject"]`).value;
+  const message = form.querySelector(`textarea[name="message"]`).value;
+
+  // if (message.value.trim() < 11) {
+  //   alert("Please enter a message");
+  //   return false;
+  // }
+
+  popUp();
+
+  return false;
+};
+
+const popUp = () => {
+  const popUp = document.getElementById("popUp");
+  const loader = document.getElementById("loader");
+  const success = document.getElementById("success");
+
+  popUp.classList.add("show");
+
+  setTimeout(function () {
+    loader.style.display = "none";
+    success.style.display = "flex";
+
+    // Redirect after 10 seconds
+    setTimeout(function () {
+      window.location.href = "index.html";
+    }, 2100);
+  }, 1000);
+};
+
+// // Nav bar script
+// window.addEventListener("scroll", function () {
+//   var navbar = document.querySelector(".container");
+//   var navBarOffset = navbar.offsetTop;
+
+//   if (window.pageYOffset >= navBarOffset) {
+//     navbar.classList.add("fixed-nav");
+//   } else {
+//     navbar.classList.remove("fixed-nav");
+//   }
+// });
